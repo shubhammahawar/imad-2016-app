9a07e709706068dd8bc087a4841f67f95eb23de0
shubhammahawar <shubham.mahawar1001@gmail.com>
2016-09-28 00:25:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req, res){
-res.send("article 1 is requested and is served here");
+res.send("Sachin tendulkar will come back");
 });
 
 app.get('/article-two',function (req, res){
