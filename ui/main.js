7eed949c1e7d56d15f9c87d34fb181d075a259bb
shubhammahawar <shubham.mahawar1001@gmail.com>
2016-09-30 13:39:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,12 @@ element.Innerelement = 'New value';
  //move image
  
  var img=document.getElementById('madi');
+ var marginLeft=0;
+ function moveRight(){
+     marginLeft=marginLeft + 10;
+     img.style.marginLeft=marginLeft + 'px';
+ }
  img.onclick = function() {
- img.style.marginleft='100px';
+ var interval = setinterval(moveRight,100);
  };
+ 
