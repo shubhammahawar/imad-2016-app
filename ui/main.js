c8542e1bac1c73b8cc3beb8c2fc220326686506d
shubhammahawar <shubham.mahawar1001@gmail.com>
2016-10-06 18:22:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,2 @@
 console.log('Loaded!');
-//change the text of the main-text div
 
-var element=document.getElementById('main-text');
-
-element.Innerelement = 'New value';
- //move image
- 
- var img=document.getElementById('madi');
- var marginLeft=0;
- function moveRight(){
-     marginLeft=marginLeft + 10;
-     img.style.marginLeft=marginLeft + 'px';
- }
- img.onclick = function() {
- var interval = setinterval(moveRight,100);
- };
- 
