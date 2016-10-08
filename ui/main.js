8a92115937e96c148cba71e2c32142eee2d07d6e
shubhammahawar <shubham.mahawar1001@gmail.com>
2016-10-08 13:16:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML= 'Module p5 ID cHanges';
+
+var img=document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft='100px';
+};
