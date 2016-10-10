4bf86524f21f75b2e5d5168b00411e6b09816752
shubhammahawar <shubham.mahawar1001@gmail.com>
2016-10-10 20:31:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,15 +17,16 @@ img.onclick = function () {
     var interval = setInterval(moveRight,50);
     
 };
-
+submit.onclick = function(){
+     var request= new XMLHttpRequest();
+    request.onreadystatechange = function(){
+      if(request.readystate==XMLHttpRequest.Done){
+          if(request.status==200){
 var button = document.getElementById('counter');
 var counter=0;
 button.onclick = function(){
     
-    var request= new XMLHttpRequest();
-    request.onreadystatechange = function(){
-      if(request.readystate==XMLHttpRequest.Done){
-          if(request.status==200){
+   
               var counter = request.responseText;
                var span = document.getElementById('count');
     span.innerHTML=counter.toString();
@@ -37,18 +38,31 @@ button.onclick = function(){
    request.send(null);
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
-submit.onclick = function(){
+submit.onclick = function() {
+    
+     var request= new XMLHttpRequest();
+     
+    request.onreadystatechange = function(){
+      if(request.readystate==XMLHttpRequest.Done) {
+          
+          if(request.status==200) {
+              
     var names=request.responseText;
     names=JSON.parse(names);
     var list='';
-    for(var i =0;i<names.length;i++)
-    {
+    for(var i =0;i<names.length;i++){
+    
     list += '<li>' + names[i] + '</li>';
 }
 var ul = document.getElementById('namelist');
 ul.innerHTML = list;
+}
+}
 };
-
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+ request.open('GET','http://shubhammahawar.imad.hasura-app.io/counter',true);
+   request.send(null);
+};
