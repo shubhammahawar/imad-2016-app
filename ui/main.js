67e248cbb32e569c679153c015eb74eebc580f34
shubhammahawar <shubham.mahawar1001@gmail.com>
2016-10-10 20:36:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,16 +17,12 @@ img.onclick = function () {
     var interval = setInterval(moveRight,50);
     
 };
-submit.onclick = function(){
+button.onclick = function(){
      var request= new XMLHttpRequest();
     request.onreadystatechange = function(){
       if(request.readystate==XMLHttpRequest.Done){
           if(request.status==200){
-var button = document.getElementById('counter');
-var counter=0;
-button.onclick = function(){
-    
-   
+              
               var counter = request.responseText;
                var span = document.getElementById('count');
     span.innerHTML=counter.toString();
